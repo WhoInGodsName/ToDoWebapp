9875ee2cac29c0754da873ed68b80bc28ffa69dc
Ethan <ethan.p@equinimcollege.com>
2026-04-30 14:17:00 +0800

    Change to taskSchema

--- backend/index.js
+++ backend/index.js
@@ -54,7 +54,7 @@ const taskSchema = new mongoose.Schema({
     completed: {type: Boolean, required: true, default: false},
     description: {type: String, required: true}, //Might need to be of type Text <-
     dueDate: {type: Date, required: true},
-    dateCreated: {type: Date, required: true, default: (Date.now())}
+    dateCreated: {type: Date, required: true, default: new Date(Date.now())}
 });
 
 //define indexes for sorting
